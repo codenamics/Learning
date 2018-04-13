2ace3fec35bec626b1b479ff0c5e84436048c961
codenamics <info@codenamics.pl>
2018-04-13 23:12:44 +0200

    up

--- LoanCalculator/loancalculator/app.js
+++ LoanCalculator/loancalculator/app.js
@@ -1,27 +1,30 @@
-const form = document.getElementById('form');
-
-const loanAmount = document.getElementById('loan-amount').value;
-const interest = document.getElementById('interest').value;
-const years = document.getElementById('years').value;
-
 const btn = document.getElementById('calculate');
 
 
-const totalPayment = document.getElementById('total-payment')
-const totalInterest = document.getElementById('total-interest')
-
-
-
 btn.addEventListener('click', () => {
+
     const loanAmount = document.getElementById('loan-amount').value;
     const interest = document.getElementById('interest').value;
     const years = document.getElementById('years').value;
 
     const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
 
-    let result = Number(loanAmount) + Number(interest) + Number(years);
+    const principal = parseFloat(loanAmount);
+    const calculateInterest = parseFloat(interest) / 100 / 12;
+    const calculatePayments = parseFloat(years) * 12;
 
-    monthlyPayment.value = result;
+    const x = Math.pow(1 + calculateInterest, calculatePayments);
+    const monthly = (principal*x *calculateInterest)/(x-1);
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatePayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatePayments) - principal).toFixed(2);
+    } else {
+        M.toast({html: 'Ups something went wrong. Please check fields'})
+    }
 
 
 
