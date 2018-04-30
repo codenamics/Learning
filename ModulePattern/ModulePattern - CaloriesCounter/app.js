f44edd4d50532fbe303312b3141e2e3fb49490bc
Codenamics <info@codenamics.pl>
2018-04-30 22:59:31 +0200

    up

--- ModulePattern/ModulePattern - CaloriesCounter/app.js
+++ ModulePattern/ModulePattern - CaloriesCounter/app.js
@@ -13,9 +13,9 @@ const ItemCtrl = (function() {
     // Data Structure / State
     const data = {
         items: [
-            { id: 0, name: 'Steak Dinner', calories: 1200 },
-            { id: 1, name: 'Cookie', calories: 400 },
-            { id: 2, name: 'Eggs', calories: 300 }
+            // { id: 0, name: 'Steak Dinner', calories: 1200 },
+            // { id: 1, name: 'Cookie', calories: 400 },
+            // { id: 2, name: 'Eggs', calories: 300 }
         ],
         currentItem: null,
         totalCalories: 0
@@ -48,6 +48,17 @@ const ItemCtrl = (function() {
         },
         logData: function() {
             return data;
+        },
+        getTotalCalories: function() {
+            let total = 0;
+
+            data.items.forEach(function(item) {
+                total += item.calories
+
+            });
+            data.totalCalories = total;
+
+            return data.totalCalories
         }
     }
 })();
@@ -60,7 +71,8 @@ const UICtrl = (function() {
         itemList: '#item-list',
         addBtn: '.add-btn',
         itemNameInput: '#item-name',
-        itemCaloriesInput: '#item-calories'
+        itemCaloriesInput: '#item-calories',
+        totalCalories: '.total-calories'
     }
 
     // Public methods
@@ -88,7 +100,39 @@ const UICtrl = (function() {
         },
         getSelectors: function() {
             return UISelectors;
+        },
+        addListItem: function(item) {
+            //Show list
+            document.querySelector(UISelectors.itemList).style.display = 'block'
+                //Create list item element
+            const li = document.createElement('li');
+            li.className = 'collection-item';
+            li.id = `item-${item.id}`;
+
+            //Add HTML
+            li.innerHTML = `
+            <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content">
+              <i class="edit-item fa fa-pencil"></i>
+            </a>
+            `;
+            //Insert Item
+            document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeEnd', li)
+        },
+        clearfields: function() {
+
+            document.querySelector(UISelectors.itemNameInput).value = ''
+            document.querySelector(UISelectors.itemCaloriesInput).value = '';
+
+
+        },
+        hideList: function() {
+            document.querySelector(UISelectors.itemList).style.display = 'none'
+        },
+        showTotalCalories: function(calories) {
+            document.querySelector(UISelectors.totalCalories).textContent = calories
         }
+
     }
 })();
 
@@ -114,7 +158,20 @@ const App = (function(ItemCtrl, UICtrl) {
         if (input.name !== '' && input.calories !== '') {
             // Add item
             const newItem = ItemCtrl.addItem(input.name, input.calories);
+
+            //Add item to UI
+            UICtrl.addListItem(newItem);
+
+            //Get total calories
+
+            const totalCalories = ItemCtrl.getTotalCalories();
+
+            //Add totalcaloires to UI
+            UICtrl.showTotalCalories(totalCalories);
+            //Clear fields
+            UICtrl.clearfields()
         }
+
 
         e.preventDefault();
     }
@@ -125,9 +182,17 @@ const App = (function(ItemCtrl, UICtrl) {
             // Fetch items from data structure
             const items = ItemCtrl.getItems();
 
-            // Populate list with items
-            UICtrl.populateItemList(items);
+            //Check if any items
+            if (items.length === 0) {
+                UICtrl.hideList()
+            } else {
+                // Populate list with items
+                UICtrl.populateItemList(items);
+            }
+            const totalCalories = ItemCtrl.getTotalCalories();
 
+            //Add totalcaloires to UI
+            UICtrl.showTotalCalories(totalCalories);
             // Load event listeners
             loadEventListeners();
         }
