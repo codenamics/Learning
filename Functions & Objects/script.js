8349110ca2a72b473d14c230dfe82a7cf60e40a1
codenamics <info@codenamics.pl>
2018-05-10 19:33:13 +0200

    up

--- Functions & Objects/script.js
+++ Functions & Objects/script.js
@@ -62,23 +62,87 @@
 
 // console.log(heart)
 
-function interviewQuestion(job){
-    if(job === 'designer'){
-        return function(name){
-            console.log(name + ', can you explain what is UX?')
-        }
-    }else if(job === 'teacher'){
-        return function(name){
-            console.log(name + ', what is 2 + 2?')
-        }
+// function interviewQuestion(job){
+//     return function(name){
+//         if (job === 'developer') {
+//             console.log(name + ' what is closures?')
+//         }else if(job === 'designer'){
+//             console.log(name + ' what is UX?')
+//         }
+//     }
+
+// }
+
+// let teacherQuestion = interviewQuestion('developer')
+// teacherQuestion('John')
+// let ux = interviewQuestion('designer')
+// ux('Annie')
+
+// function retirement(retirementAge){
+//     let a = ' years left to retirement'
+//     return function(yearofbirth){
+//         let age = 2018 - yearofbirth;
+//         console.log((retirementAge - age) + a)
+//     }
+// }
+
+// let poland = retirement(67)
+// poland(1989)
+
+// let john = {
+//     name: 'John',
+//     age: '26',
+//     job: 'teacher',
+//     presentation: function(style, time){
+//         if (style === 'friendly') {
+//             console.log(`Hi I am ${this.name}, ${this.age},and I am a ${this.job}. Good ${time}`)
+//         }else if(style === 'formal'){
+//             console.log(`Hey I'm ${this.name}, ${this.age},and I am a ${this.job}. Good ${time}`)
+//         }
+//     }
+// }
+
+// john.presentation('formal', 'morning')
+
+// let emily = {
+//     name: 'emily',
+//     age: '28',
+//     job: 'designer'
+// }
+
+// john.presentation.call(emily, 'formal', 'morning')
+
+let Question = function(question, answers, correct){
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct
+}
+
+Question.prototype.displayAnswer = function(){
+    console.log(this.question);
+    this.answers.forEach((answer, index) => {
+        console.log(`${index}: ${answer}`)
+    });
+}
+ 
+Question.prototype.showAnswer = function(ans){
+    if(ans === this.correct){
+console.log('Correct answer')
     }else{
-        return function(name){
-             console.log('Hello' + name)
-        }
+        console.log('Wrong answer')
     }
 }
 
-let teacherQuestion = interviewQuestion('teacher')
-teacherQuestion('John')
-let ux = interviewQuestion('designer')
-ux('Annie')
+let q1 = new Question('Is JS the best language?', ['yes', 'no'], 0)
+let q2 = new Question('My name is?', ['Damian', 'Annie', 'Mark'], 0)
+let q3 = new Question('What is the color of the sky?', ['red','blue'], 1)
+
+let questions = [q1,q2,q3]
+
+ let n = Math.round(Math.random() * questions.length);
+
+questions[n].displayAnswer()
+
+let answer = parseInt(prompt('Please select answer'))
+
+questions[n].showAnswer(answer)
